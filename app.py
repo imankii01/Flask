278f913d72e256469b8b4e60b77fa3f92991d701
Ankit <private.ankit047@gmail.com>
2025-02-15 00:37:11 +0530

    GET API

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello, World!'})
+
+
+@app.route('/hii', methods=['GET'])
+def hii():
+    return jsonify({'message': 'Hii, World!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
